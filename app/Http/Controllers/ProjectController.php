bbf0744979fcb64cdf6dc68c3d55ab99f82cd193
develop-erre <richardfoncea@gmail.com>
2020-06-29 23:40:57 -0400

    Elocuent eliminar registros

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -155,8 +155,11 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        //
+        //Project::destroy($id);
+        $project->delete();
+
+        return redirect()->route('projects.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contacto', 'contact')->name('contacto');
+
 Route::get('/portafolio','ProjectController@index')->name('projects.index');
-Route::post('/portafolio','ProjectController@store')->name('projects.store');
 Route::get('/portafolio/crear','ProjectController@create')->name('projects.create');
+
 Route::get('/portafolio/{project}/editar','ProjectController@edit')->name('projects.edit');
 Route::patch('/portafolio/{project}','ProjectController@update')->name('projects.update');
 
+Route::post('/portafolio','ProjectController@store')->name('projects.store');
 Route::get('/portafolio/{project}','ProjectController@show')->name('projects.show');
+
+Route::delete('/portafolio/{project}','ProjectController@destroy')->name('projects.destroy');
 
 //Route::resource('portafolio', 'PortafolioController@index');
 //nombre ruta URL                   Controlador                   nombre ruta invocada en el actions
